76e30238e2b9b2fc653c5fbe29c076a9d1edd83d
LorenzoMegna24 <megnalorenzo0@gmial.com>
2023-04-21 16:41:48 +0200

    Esercizio base completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,21 @@ const { createApp } = Vue
 
 createApp({
   data(){
-
+    return {
+      emailRandom: []
+    }
   },
   mounted(){
-    axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res)=>{
-      console.log(res.data.response);
-    })
+
   },
   methods:{
-
+    myFunction(){
+      for (let i = 0; i < 10; i++) {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res)=>{
+          console.log(res.data.response);
+          this.emailRandom.push( res.data.response ) 
+        })
+      }
+    }
   }
 }).mount('#app')
